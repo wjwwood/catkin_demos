086b2d58d7071a803c32e5d570d2ff02585b49b6
William Woodall <william@osrfoundation.org>
2013-04-02 15:08:19 -0700

    Adding example of gtest with data files

---
+++ test_with_data/include/test_with_data/add.h
@@ -0,0 +1,4 @@
+
+namespace test_with_data {
+    int add(int &operand1, int &operand2);
+}
---
+++ test_with_data/test/test_with_data2.cpp
@@ -0,0 +1,32 @@
+#include <fstream>
+#include <string.h>
+
+#include "gtest/gtest.h"
+
+#include "test_with_data/add.h"
+
+namespace {
+
+class SomeTests : public ::testing::Test {};
+
+TEST_F(SomeTests, test_add) {
+  std::fstream in;
+  in.open("data/test.dat", std::fstream::in);
+  int op1, op2, expected;
+  while (in >> op1 >> op2 >> expected) {
+    EXPECT_EQ(expected, test_with_data::add(op1, op2));
+  }
+  in.close();
+}
+
+}
+
+int main(int argc, char **argv) {
+  try {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+  } catch (std::exception &e) {
+    std::cerr << "Unhandled Exception: " << e.what() << std::endl;
+  }
+  return 1;
+}
